101b25b883e8a3458a5d905177f7788886e9e4c3
Saktiaari <rizkyari6408@gmail.com>
2024-01-31 09:04:23 +0700

    cilik kabeh

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From cilik kabeh" }');
+    res.send('{ "response": "Hello From cilik kabe" }');
 });
 
 app.get('/will', function (req, res) {
